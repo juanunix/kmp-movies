aca0773d915e3ab63d69463a34f422089542e16c
Juan Sanz <juanunix@gmail.com>
2025-03-12 20:16:46 -0400

    :fire: Remove unnecessary code

--- composeApp/src/commonMain/kotlin/org/juansanz/kmpmovies/Platform.kt
+++
@@ -1,7 +0,0 @@
-package org.juansanz.kmpmovies
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
--- composeApp/src/wasmJsMain/kotlin/org/juansanz/kmpmovies/Platform.wasmJs.kt
+++
@@ -1,7 +0,0 @@
-package org.juansanz.kmpmovies
-
-class WasmPlatform: Platform {
-    override val name: String = "Web with Kotlin/Wasm"
-}
-
-actual fun getPlatform(): Platform = WasmPlatform()
