630a69868d4f5ec22d5d75f2a319524503f3e99d
Juan Sanz <juanunix@gmail.com>
2025-03-13 11:54:01 -0400

    fix imports

--- composeApp/src/iosMain/kotlin/org/juansanz/kmpmovies/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/juansanz/kmpmovies/MainViewController.kt
@@ -1,7 +1,10 @@
 package org.juansanz.kmpmovies
 
+import App
 import androidx.compose.ui.window.ComposeUIViewController
 import org.juansanz.kmpmovies.data.database.MoviesDatabase
+import org.juansanz.kmpmovies.data.database.getRoomDatabase
+import org.juansanz.kmpmovies.database.getDatabaseBuilder
 
 fun MainViewController() = ComposeUIViewController {
     val database: MoviesDatabase = getRoomDatabase(getDatabaseBuilder())
--- composeApp/src/iosMain/kotlin/org/juansanz/kmpmovies/database/database.kt
+++ composeApp/src/iosMain/kotlin/org/juansanz/kmpmovies/database/database.kt
@@ -4,6 +4,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import org.juansanz.kmpmovies.data.database.DATABASE_NAME
 import org.juansanz.kmpmovies.data.database.MoviesDatabase
+import org.juansanz.kmpmovies.data.database.instantiateImpl
 import platform.Foundation.NSHomeDirectory
 
 fun getDatabaseBuilder(): RoomDatabase.Builder<MoviesDatabase> {
