616d21de5684322536704a6d5273b01be2463518
Juan Sanz <juanunix@gmail.com>
2025-03-12 20:50:14 -0400

    :fire: Extract Home

---
+++ composeApp/src/commonMain/kotlin/org/juansanz/kmpmovies/ui/screens/Screen.kt
@@ -0,0 +1,17 @@
+package org.juansanz.kmpmovies.ui.screens
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Screen(content: @Composable () -> Unit) {
+    MaterialTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            content = content
+        )
+    }
+}
